ffe9c5b9f55da2cffc0884e36ebb461ebc7fec57
Darren <DarrenLeel33@gmail.com>
2021-05-21 16:57:04 -0400

    processing project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let newTaskForm = document.getElementById('create-task-form')
+
+  let newTaskDescription = document.getElementById('new-task-description')
 });
+
+
+function renderAddTask(){
+  document.querySelector('#new-task-description').textContent=console.log(document.querySelector('.task'))
+
+  let taskLi = document.createElement('li')
+
+  
+
+}
+
+
+
+document.querySelector("#Create New Task").addEventListener('submit', (e) => {
+  e.preventDefault()
+  let taskForm = {
+      task: e.target.newTaskDescription.value
+  }
+  console.log(taskForm)
+})
+
+//taskList.createNewTask(newTaskDescription.value);
